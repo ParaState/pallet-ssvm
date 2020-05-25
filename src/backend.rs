82f9342d2c15a129102a014d0f6f2ea1931157b4
dm4 <dm4@secondstate.io>
2020-05-25 16:57:07 +0000

    [License] Update license.

--- src/backend.rs
+++ src/backend.rs
@@ -1,3 +1,19 @@
+// Copyright (C) 2020 Second State.
+// This file is part of Pallet-SSVM.
+
+// Pallet-SSVM is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+
+// Pallet-SSVM is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 use crate::{AccountCodes, AccountStorages, Accounts, Event, Module, Trait};
 use codec::{Decode, Encode};
 use evm::backend::{Apply, ApplyBackend, Backend as BackendT};
